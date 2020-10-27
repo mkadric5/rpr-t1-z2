83d8e4d05e9612ce3eea1687f3e94e1eeb0d568e
mkadric5 <mkadric5@etf.unsa.ba>
2020-10-27 16:14:07 +0000

    Implementirana funkcija sumaCifara

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -2,7 +2,19 @@ package ba.unsa.etf.rpr;
 
 public class Main {
 
+    public static int sumaCifara (int n)
+    {
+        int cifra, suma = 0;
+        do {
+            cifra = n % 10;
+            suma = suma + cifra;
+            n = n / 10;
+        }while (n>0);
+
+        return suma;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
