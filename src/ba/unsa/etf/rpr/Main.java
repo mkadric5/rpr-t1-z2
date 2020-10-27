69cbb09c430a846077ee3177313fff2771be8a9c
mkadric5 <mkadric5@etf.unsa.ba>
2020-10-27 16:18:48 +0000

    Dodano tijelo main funkcije

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static int sumaCifara (int n)
@@ -9,12 +11,17 @@ public class Main {
             cifra = n % 10;
             suma = suma + cifra;
             n = n / 10;
-        }while (n>0);
+        } while (n>0);
 
         return suma;
     }
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner (System.in);
+        System.out.print("Unesite broj n: ");
+        int n = in.nextInt();
+        System.out.println(" Brojevi izmedju 1 i " + n + " koji su djeljivi sumom svojih cifara su: ");
+        for (int i=1; i<=n; i++)
+            if (i % sumaCifara(i) == 0) System.out.println(i + " ");
     }
 }
